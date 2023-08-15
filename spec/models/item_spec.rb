949bc38b0d0276baa714471ebe81d0aa5d04bb1e
shiotanishi <139023486+shiotanishi@users.noreply.github.com>
2023-08-15 17:49:56 +0900

    モデルの単体テストコード記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -117,3 +117,5 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price is out of setting range")
         end
+      end
+  end
