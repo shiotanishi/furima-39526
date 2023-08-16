bc8c8b55e3b32fa1d742e35947537dccb63c07b4
shiotanishi <139023486+shiotanishi@users.noreply.github.com>
2023-08-16 21:14:20 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show]
+  # before_action :set_item, only: [:edit, :show]
+  before_action :authenticate_user!, only: [:new]
+
+
 
   def index
     @item = Item.all
@@ -7,38 +10,51 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.attributes = {
+      item_name: params[:item_name],
+      info: params[:info],
+      category_id: params[:category_id],
+      condition_id: params[:condition_id],
+      shipping_fee_id: params[:shipping_fee_id],
+      prefecture_id: params[:prefecture_id],
+      scheduled_delivery_id: params[:scheduled_delivery_id]
+    }
   end
 
   def create
     @item =Item.create(item_params)
-    redirect_to '/'
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
-  end
-
-  def edit
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to root_path
-  end
-
-  def update_user
-    if current_user.update(user_params)
+    if @item.save
       redirect_to root_path
     else
-      render :edit, status: :unprocessable_entity
+           render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-  end
+  # def destroy
+  #   @item = Item.find(params[:id])
+  #   @item.destroy
+  #   redirect_to root_path
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   @item = Item.find(params[:id])
+  #   @item.update(item_params)
+  #   redirect_to root_path
+  # end
+
+  # def update_user
+  #   if current_user.update(user_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def show
+  # end
 
   private
 
@@ -46,13 +62,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :info, :price, :image, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
-  def move_to_index
-    return if user_signed_in?
+  # def move_to_inde
+  #   return if user_signed_in?
 
-    redirect_to action: :index
-  end
+  #   redirect_to action: :index
+  # end
 end
