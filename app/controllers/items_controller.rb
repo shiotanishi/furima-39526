87ccbe8a27bde5f3f3b3810a631271ef915b8025
shiotanishi <139023486+shiotanishi@users.noreply.github.com>
2023-08-19 16:09:10 +0900

    編集画面のビュー表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,12 +35,8 @@ class ItemsController < ApplicationController
   # end
 
   def edit
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @item = Item.new
+  
   
   end
 
