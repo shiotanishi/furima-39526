27d2de21e8cc1b7f2acdf33dd2802f269e24a6d5
shiotanishi <139023486+shiotanishi@users.noreply.github.com>
2023-08-25 14:09:29 +0900

    デプロイ

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,7 +18,7 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
+        // debugger;
       }
       numberElement.clear();
       expiryElement.clear();
